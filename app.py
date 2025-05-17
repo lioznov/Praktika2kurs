3d5a0538cb1adf611d9cd1e6f44f73e7ef5b8af0
lioznov <andreylioznov@yandex.ru>
2025-05-17 10:21:24 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,52 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import sys
+import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///praktika.db'
-db = SQLAlchemy(app)
+# Добавляем путь к директории Praktika в PYTHONPATH
+current_dir = os.path.dirname(os.path.abspath(__file__))
+summer_practic_dir = os.path.join(current_dir, 'Praktika')
+sys.path.append(summer_practic_dir)
 
+# Импорт app и db из config.py
+from config import app, db
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
+# Также импортируем класс Post
+from config import Post
 
-@app.route("/index") # получаем доступ к главной странице
+# Создание контекста приложения
+app.app_context().push()
+
+# Создание всех таблиц в базе данных
+db.create_all()
+
+@app.route("/index")  # получаем доступ к главной странице
 @app.route("/")
 def index():
     return render_template('index.html')
 
-
 @app.route("/posts")
 def posts():
     posts = Post.query.all()
-    return render_template('posts.html',posts=posts)
+    return render_template('posts.html', posts=posts)
 
-
-@app.route("/create",methods=['POST','GET'])
+@app.route("/create", methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
         title = request.form['title']
         text = request.form['text']
 
-        post = Post(title=title,text=text)
+        post = Post(title=title, text=text)
 
         try:
             db.session.add(post)
             db.session.commit()
             return redirect('/')
         except:
-            return 'При добавлении статье произошла ошибка!'
+            return 'При добавлении статьи произошла ошибка!'
         return redirect('/')
     else:
         return render_template('create.html')
-
 
 @app.route("/about")
 def about():
---
+++ config.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'praktika.db')
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
